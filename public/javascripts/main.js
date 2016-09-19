611af9b5e0f56e205e6019783a64ee9f51f7dd0b
CBorum <christopherborum@gmail.com>
2016-09-20 01:40:35 +0200

    signup

---
+++ public/javascripts/main.js
@@ -0,0 +1,34 @@
+/**
+ * Created by ChristopherBorum on 19/09/16.
+ */
+Vue.config.delimiters = ['${', '}']
+Vue.config.debug = true
+
+var vm = new Vue({
+    el: '#app',
+    data: {
+        email: "",
+        signedUp: false
+    },
+    methods: {
+        onSubmit: function () {
+            // this.signedUp = true
+            var signup = {
+                email: this.email,
+                timestamp: new Date().getTime()
+            }
+            console.log(signup)
+            Vue.http.post('/', signup).then(function (response) {
+                if (response.body === 'ok') {
+                    this.signedUp = true
+                    // todo better feedback
+                    alert('Signed up')
+                } else {
+                    alert(response.body)
+                }
+            }, function (response) {
+                console.log(response)
+            })
+        }
+    }
+})
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('../db/mongo')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Traverse' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'traverse'});
 });
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+router.post('/', function (req, res, next) {
+    var signup = req.body;
+    if (validateEmail(signup.email)) {
+        // documents can be overwritten?
+        mongo.get().collection('signup').insertOne(signup, function (err, result) {
+            if (err) {
+                res.end(err)
+            }
+            res.end('ok')
+        })
+    } else {
+        console.log("invalid email")
+        res.end('invalid email')
+    }
+})
 
 module.exports = router;
